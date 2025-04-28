502fc03db425d36d03c39cc8db3e120033990480
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-04-28 13:06:48 -0700

    Updated for each

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ArrayList<Product> list = new ArrayList<>();
 //      Product laptop = new Product("Laptop", 1200000);
         list.add(new Product("Laptop", 120000));
         list.add(new Product("Phone", 100000));
         list.add(new Product("Headphones", 70000));
-        list.remove(1);
-        for(Product product : list){
-            System.out.println("Product name: " + product.name);
-            System.out.println("Product Price: " + product.price);
-            System.out.println();
-
-
-        }
+//        list.remove(1);
+        int number = 1;
+//        for(Product product : list){
+//            System.out.println("Product name: " + product.name);
+//            System.out.println("Product Price: " + product.price);
+//            System.out.println();
+//    }
+//
+for(Product product : list){
+    System.out.println(number + " ." + "Product Name: "+ product.name +" " + product.price);
+    number++;
+}
     }
 }

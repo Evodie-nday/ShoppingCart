85604877001615d0e0975c7874ef29151e473c9f
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-04-28 12:58:28 -0700

    created a shopping cart using generics i added, removed and numbered items.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList<Product> list = new ArrayList<>();
+//      Product laptop = new Product("Laptop", 1200000);
+        list.add(new Product("Laptop", 120000));
+        list.add(new Product("Phone", 100000));
+        list.add(new Product("Headphones", 70000));
+        list.remove(1);
+        for(Product product : list){
+            System.out.println("Product name: " + product.name);
+            System.out.println("Product Price: " + product.price);
+            System.out.println();
+
+
+        }
+    }
+}
